f3eae7c3baab3752e76567369e36c2f7070eb9a6
Chinmay Dalvi <chinmaydalvi94@gmail.com>
2017-10-20 12:19:31 +0530

    added axios in project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
 const API_KEY = 'af45e1ee4bcdc8e259a8978de0426750'
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+export function fetchWeather(city){
+
+	const url = `${ROOT_URL}&q=${city},us`;
+
+	const request = axios.get(url)
+
+	return{
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
